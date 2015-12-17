30891c3d391ad85387799e22aee0e9dda552efc6
dragonpt <cesar.maximo@gmail.com>
2015-12-17 23:18:47 +0000

    ALSPS/Light Sensor: EPL2182-new: Fix LUX Values, smooth the Transitions

--- drivers/misc/mediatek/mach/mt6752/h560/alsps/EPL2182-new/cust_alsps.c
+++ drivers/misc/mediatek/mach/mt6752/h560/alsps/EPL2182-new/cust_alsps.c
@@ -5,13 +5,14 @@
 static struct alsps_hw cust_alsps_hw = {
     .i2c_num    = 1,
     .polling_mode_ps =0,
+    .polling_mode_als =1,
     .power_id   = MT65XX_POWER_NONE,    /*LDO is not used*/
     .power_vol  = VOL_DEFAULT,          /*LDO is not used*/
-	.i2c_addr   = {0x0C, 0x48, 0x78, 0x00},
-	.als_level	= {20, 40, 150, 300, 500, 700, 1150, 2250, 4500, 8000, 15000, 30000, 50000},
-	.als_value	= {100, 400, 700, 1000, 1300, 1800, 2500, 4000, 8000, 10000, 10000, 10000, 10000, 10000},
-	.ps_threshold_low = 300,
-	.ps_threshold_high = 400,
+    //.i2c_addr   = {0x0C, 0x48, 0x78, 0x00},
+    .als_level  = {0, 111, 231, 471, 700, 1058, 1703, 3883, 10171, 10443, 15445, 28499, 35153, 41421, 59194, 65535, 65535, 65535},
+    .als_value  = {0, 30, 66, 133, 211, 303, 501, 1002, 2001, 3355, 5001, 8008, 10010, 12000, 16010, 20010, 20010, 20010, 20010},
+    .ps_threshold_high = 300,
+    .ps_threshold_low = 200,
 };
 struct alsps_hw *epl2182_get_cust_alsps_hw(void) {
     return &cust_alsps_hw;
