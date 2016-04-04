2cfb5ebf9235ede572276bb782437905da39e797
DerTeufel <dominik-kassel@gmx.de>
2016-04-04 15:49:46 +0200

    fastcharge

--- drivers/power/mediatek/charge_level.c
+++ drivers/power/mediatek/charge_level.c
@@ -23,7 +23,7 @@ static ssize_t charge_level_ac_show(struct kobject *kobj, struct kobj_attribute 
 {
 
 	// print current value
-	return sprintf(buf, "%d\n", ac_level / 100 );
+	return sprintf(buf, "%d", ac_level / 100 );
 
 }
 
@@ -60,7 +60,7 @@ static ssize_t charge_level_usb_show(struct kobject *kobj, struct kobj_attribute
 {
 
 	// print current value
-	return sprintf(buf, "%d\n", usb_level / 100);
+	return sprintf(buf, "%d", usb_level / 100);
 
 }
 
@@ -95,8 +95,7 @@ static ssize_t charge_level_usb_store(struct kobject *kobj, struct kobj_attribut
 static ssize_t quick_charge_enable_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	// print current value
-	return sprintf(buf, "%d\n", qc_enable);
-
+	return sprintf(buf, "%d", qc_enable);
 }
 
 
