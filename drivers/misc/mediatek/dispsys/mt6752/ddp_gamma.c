72bfbad4d1bd55849bc37fcdf7b4ffdf7c1c8767
Diogo Ferreira <defer@cyngn.com>
2016-04-07 15:11:28 +0100

    mt6752: Add an RGB configuration sys attr driver for videox/corr10
    Adds the RGB configuration node which leverages the gamma subsystem to
    perform color correction.
    
    The new node takes rgb triplets that range from 0-2000 each, computes
    the gamma registers and dispatches them to the gamma correction
    subsystem.
    
    Change-Id: Iac9d3cbd4f423ccfffb8d665c29cfd251767a398

--- drivers/misc/mediatek/dispsys/mt6752/ddp_gamma.c
+++ drivers/misc/mediatek/dispsys/mt6752/ddp_gamma.c
@@ -111,10 +111,18 @@ static int disp_gamma_set_lut(const DISP_GAMMA_LUT_T __user *user_gamma_lut, voi
         return -EFAULT;
     }
     
+#ifdef CONFIG_MTK_VIDEOX_CYNGN_LIVEDISPLAY
+    if (virt_addr_valid(user_gamma_lut)) {
+        memcpy(gamma_lut, user_gamma_lut, sizeof(DISP_GAMMA_LUT_T));
+    } else
+#endif
     if (copy_from_user(gamma_lut, user_gamma_lut, sizeof(DISP_GAMMA_LUT_T)) != 0) {
+        printk(KERN_ERR "[GAMMA] disp_gamma_set_lut: cannot copy from user mem\n");
         ret = -EFAULT;
         kfree(gamma_lut);
-    } else {
+    }
+
+    if (!ret) {
         id = gamma_lut->hw_id;
         if (0 <= id && id < DISP_GAMMA_TOTAL) {
             spin_lock(&g_gamma_global_lock);
