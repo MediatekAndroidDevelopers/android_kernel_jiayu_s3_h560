c3d0632b5afd44784f316ded51889bc3a353870e
Cesar Matias <cesar.maximo@gmail.com>
2017-02-14 00:30:46 +0000

    Google Security Patch: [CVE-2016-10044] Elevation of privilege vulnerability in kernel file system

--- fs/aio.c
+++ fs/aio.c
@@ -1165,6 +1165,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 	struct kioctx *ctx;
 	long ret = 0;
 	int i = 0;
+        struct blk_plug plug;
 
 	if (unlikely(nr < 0))
 		return -EINVAL;
@@ -1181,6 +1182,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		return -EINVAL;
 	}
 
+        blk_start_plug(&plug);
 
 	/*
 	 * AKPM: should this return a partial result if some of the IOs were
@@ -1204,6 +1206,7 @@ long do_io_submit(aio_context_t ctx_id, long nr,
 		if (ret)
 			break;
 	}
+        blk_finish_plug(&plug);
 
 	put_ioctx(ctx);
 	return i ? i : ret;
